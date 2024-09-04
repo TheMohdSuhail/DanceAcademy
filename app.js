fcb60dbf69ca04db9452c27255618398fc87614b
Mohd Suhail <93874933+TheMohdSuhail@users.noreply.github.com>
2024-09-05 00:14:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 80;
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/msp');
+  await mongoose.connect(process.env.CONN);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
